143b98ae18ba014b6282b8dee8168b62071ccb2e
Mitchell Gonzalez <mitchell.gonzalez@gmail.com>
2021-02-11 00:23:17 +0000

    Buggy code is gone

--- wordsearch.js
+++ wordsearch.js
@@ -30,37 +30,37 @@ const transpose = function (matrix) {
   return finalArr;
 };
 
-const result = wordSearch(
-  [
-    ["A", "W", "F", "Q", "A", "F", "K", "L"],
-    ["S", "E", "R", "N", "F", "E", "L", "D"],
-    ["Y", "F", "A", "F", "Q", "U", "A", "L"],
-    ["H", "M", "N", "T", "E", "V", "R", "G"],
-    ["W", "H", "K", "S", "Y", "E", "R", "L"],
-    ["B", "F", "R", "E", "N", "E", "Y", "B"],
-    ["U", "B", "T", "W", "A", "P", "A", "I"],
-    ["O", "D", "C", "A", "K", "U", "A", "S"],
-    ["E", "Z", "K", "F", "Q", "U", "A", "L"],
-  ],
-  "FRANK"
-);
+// const result = wordSearch(
+//   [
+//     ["A", "W", "F", "Q", "A", "F", "K", "L"],
+//     ["S", "E", "R", "N", "F", "E", "L", "D"],
+//     ["Y", "F", "A", "F", "Q", "U", "A", "L"],
+//     ["H", "M", "N", "T", "E", "V", "R", "G"],
+//     ["W", "H", "K", "S", "Y", "E", "R", "L"],
+//     ["B", "F", "R", "E", "N", "E", "Y", "B"],
+//     ["U", "B", "T", "W", "A", "P", "A", "I"],
+//     ["O", "D", "C", "A", "K", "U", "A", "S"],
+//     ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+//   ],
+//   "FRANK"
+// );
 
-const result2 = wordSearch(
-  [
-    ["A", "W", "C", "F", "Q", "U", "A", "L"],
-    ["S", "E", "I", "N", "F", "E", "L", "D"],
-    ["Y", "F", "C", "F", "Q", "U", "A", "L"],
-    ["H", "M", "J", "T", "E", "V", "R", "G"],
-    ["W", "H", "C", "S", "Y", "E", "R", "L"],
-    ["B", "F", "R", "E", "N", "E", "Y", "B"],
-    ["U", "B", "T", "W", "A", "P", "A", "I"],
-    ["O", "D", "C", "A", "K", "U", "A", "S"],
-    ["E", "Z", "K", "F", "Q", "U", "A", "L"],
-  ],
-  "SEINFELD"
-);
+// const result2 = wordSearch(
+//   [
+//     ["A", "W", "C", "F", "Q", "U", "A", "L"],
+//     ["S", "E", "I", "N", "F", "E", "L", "D"],
+//     ["Y", "F", "C", "F", "Q", "U", "A", "L"],
+//     ["H", "M", "J", "T", "E", "V", "R", "G"],
+//     ["W", "H", "C", "S", "Y", "E", "R", "L"],
+//     ["B", "F", "R", "E", "N", "E", "Y", "B"],
+//     ["U", "B", "T", "W", "A", "P", "A", "I"],
+//     ["O", "D", "C", "A", "K", "U", "A", "S"],
+//     ["E", "Z", "K", "F", "Q", "U", "A", "L"],
+//   ],
+//   "SEINFELD"
+// );
 
-console.log(result);
-console.log(result2);
+// console.log(result);
+// console.log(result2);
 
 module.exports = wordSearch;
